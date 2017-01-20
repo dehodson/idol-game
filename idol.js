d7bff57ce7a0c74b6ee2847aed5ce25f1f858f0c
Unknown <dehodson@gmail.com>
2017-01-20 18:06:02 -0500

    Started to add drag and drop, tweaked descriptions

--- idol.js
+++ idol.js
@@ -57,29 +57,29 @@ Tiki.prototype.getDescription = function(){
 		}else if(this.sweetness > 3){
 			description += "<span class=\"sweet\">Very sweet.</span><br />";
 		}else if(this.sweetness > 2){
-			description += "<span class=\"sweet\">Somewhat sweet.</span><br />";
+			description += "<span class=\"sweet\">Fairly sweet.</span><br />";
 		}else if(this.sweetness > 1){
 			description += "<span class=\"sweet\">Kind of sweet.</span><br />";
 		}else{
-			description += "<span class=\"sweet\">Tastes a tad bit sweet.</span><br />"
+			description += "<span class=\"sweet\">Tastes a little sweet.</span><br />"
 		}
 		if(this.spiciness > 5){
 			description += "<span class=\"spicy\">Extremely spicy.</span><br />";
 		}else if(this.spiciness > 3){
 			description += "<span class=\"spicy\">Very spicy.</span><br />";
 		}else if(this.spiciness > 2){
-			description += "<span class=\"spicy\">Somewhat spicy.</span><br />";
+			description += "<span class=\"spicy\">Fairly spicy.</span><br />";
 		}else if(this.spiciness > 1){
 			description += "<span class=\"spicy\">Kind of spicy.</span><br />";
 		}else{
-			description += "<span class=\"spicy\">Burns your tongue a little.</span><br />"
+			description += "<span class=\"spicy\">Slightly hot to taste.</span><br />"
 		}
 		if(this.stinkiness > 5){
 			description += "<span class=\"stinky\">Extremely stinky.</span><br />";
 		}else if(this.stinkiness > 3){
 			description += "<span class=\"stinky\">Very stinky.</span><br />";
 		}else if(this.stinkiness > 2){
-			description += "<span class=\"stinky\">Somewhat stinky.</span><br />";
+			description += "<span class=\"stinky\">Fairly stinky.</span><br />";
 		}else if(this.stinkiness > 1){
 			description += "<span class=\"stinky\">Kind of stinky.</span><br />";
 		}else{
@@ -90,11 +90,11 @@ Tiki.prototype.getDescription = function(){
 		}else if(this.prettiness > 3){
 			description += "<span class=\"pretty\">Very pretty.</span>";
 		}else if(this.prettiness > 2){
-			description += "<span class=\"pretty\">Somewhat pretty.</span>";
+			description += "<span class=\"pretty\">Fairly pretty.</span>";
 		}else if(this.prettiness > 1){
 			description += "<span class=\"pretty\">Kind of pretty.</span>";
 		}else{
-			description += "<span class=\"pretty\">More aesthetically pleasing.</span>"
+			description += "<span class=\"pretty\">Looks nice.</span>"
 		}
 	}
 
@@ -108,6 +108,20 @@ function breedTikis(parent1, parent2, mutationChance){
 	var prettiness = ((parent1.prettiness + parent2.prettiness) / 2) + (Math.random() * mutationChance);
 
 	return new Tiki(sweetness, spiciness, stinkiness, prettiness);
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
 }
 
 //testing
