34bf31cc3486a22c9b2f2f45628c96c9045c1d94
Unknown <dehodson@gmail.com>
2017-01-20 16:03:01 -0500

    Removed unneeded functions and added tooltips

--- idol.js
+++ idol.js
@@ -1,51 +1,4 @@
 
-// Credits to Teddy Garland for the color blending function
-function blend_colors(color1, color2, percentage)
-{
-    color1 = color1 || '#000000';
-    color2 = color2 || '#ffffff';
-    percentage = percentage || 0.5;
-
-    if (color1.length != 4 && color1.length != 7)
-        throw new error('colors must be provided as hexes');
-
-    if (color2.length != 4 && color2.length != 7)
-        throw new error('colors must be provided as hexes');    
-
-    if (percentage > 1 || percentage < 0)
-        throw new error('percentage must be between 0 and 1');
-
-    if (color1.length == 4)
-        color1 = color1[1] + color1[1] + color1[2] + color1[2] + color1[3] + color1[3];
-    else
-        color1 = color1.substring(1);
-    if (color2.length == 4)
-        color2 = color2[1] + color2[1] + color2[2] + color2[2] + color2[3] + color2[3];
-    else
-        color2 = color2.substring(1);   
-
-    color1 = [parseInt(color1[0] + color1[1], 16), parseInt(color1[2] + color1[3], 16), parseInt(color1[4] + color1[5], 16)];
-    color2 = [parseInt(color2[0] + color2[1], 16), parseInt(color2[2] + color2[3], 16), parseInt(color2[4] + color2[5], 16)];
-
-    var color3 = [ 
-        (1 - percentage) * color1[0] + percentage * color2[0], 
-        (1 - percentage) * color1[1] + percentage * color2[1], 
-        (1 - percentage) * color1[2] + percentage * color2[2]
-    ];
-
-    color3 = '#' + int_to_hex(color3[0]) + int_to_hex(color3[1]) + int_to_hex(color3[2]);
-
-    return color3;
-}
-
-function int_to_hex(num)
-{
-    var hex = Math.round(num).toString(16);
-    if (hex.length == 1)
-        hex = '0' + hex;
-    return hex;
-}
-
 var Tiki = function(sweetness, spiciness, stinkiness, prettiness){
 	this.sweetness = sweetness;
 	this.spiciness = spiciness;
@@ -141,7 +94,7 @@ Tiki.prototype.getDescription = function(){
 		}else if(this.prettiness > 1){
 			description += "<span class=\"pretty\">Kind of pretty.</span>";
 		}else{
-			description += "<span class=\"pretty\">Looks extra nice.</span>"
+			description += "<span class=\"pretty\">More aesthetically pleasing.</span>"
 		}
 	}
 
@@ -156,3 +109,9 @@ function breedTikis(parent1, parent2, mutationChance){
 
 	return new Tiki(sweetness, spiciness, stinkiness, prettiness);
 }
+
+//testing
+
+var test = new Tiki(Math.random() * 5, Math.random() * 5, Math.random() * 5, Math.random() * 5);
+document.getElementById("tiki-1-tooltip").innerHTML = test.getName() + " Tiki<br />";
+document.getElementById("tiki-1-tooltip").innerHTML += test.getDescription();
